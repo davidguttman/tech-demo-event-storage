8cfa8625b54b7668c1d0b554044e21c1579e0088
David Guttman <david@davidguttman.com>
2014-02-22 22:28:00 -0800

    load tweaks

--- script/load.js
+++ script/load.js
@@ -1,40 +1,40 @@
 var Level    = require('level')
-var pj = require('post-json')
 var hq = require('hyperquest')
 var es = require('event-stream')
-var http = require('http')
-
-// http.globalAgent.maxSockets = 1000
 
 var db = Level('../db')
 
 var rs = db.createReadStream({keys: false})
+rs.on('error',function(err) {console.log('db err', err);})
 
 var nSent = 0
 var lastSent = 0
-// 94633
 setInterval(function() {
   console.log(nSent, nSent - lastSent)
   lastSent = nSent
 }, 1000)
 
 var ws = es.map(function(val, cb) {  
-  var i = nSent % 3
-  if (nSent > -94633) {
-    post('http://localhost:300'+i, val, cb)
-  } else {
+  // var i = nSent % 3
+  
+  if (nSent > 1250236) {
+    var req = hq.post('http://localhost:3000', function(err) {
+      if (err) console.log('req', err)
+    })
+    req.end(val)
     cb()
+    req.on('error', function(err) {console.log('err', err);})
   }
+
   nSent += 1
+})
+
+ws.on('error', function(err) {
+  console.log('ws err', err);
 })
 
 rs.pipe(ws)
 
-function post (host, body, cb) {
-  var opts = { host: 'localhost', port: 3000, path: '/', method: 'POST', agent: false }
-  var req = http.request(opts, function(res) {
-    res.setEncoding('utf8')
-    res.on('end', cb)
-  })
-  req.end(body)
-}
+// process.on('uncaughtException', function(err) {
+//   console.log('process err', err);
+// })
